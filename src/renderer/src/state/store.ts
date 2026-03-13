86cb6e9cd95d27bb1b1dee6882455eff2a9e9de3
Juan Martin Gordo <juanma.gordo99@gmail.com>
2026-03-13 20:09:24 -0300

    feat: add observable state store

---
+++ src/renderer/src/state/store.ts
@@ -0,0 +1,32 @@
+import type { Instrument } from '../instruments/types'
+import type { ParsedNote } from '../utils/note-parser'
+
+export interface AppState {
+  currentInstrument: Instrument
+  noteSequence: ParsedNote[]
+}
+
+type Listener = (state: AppState) => void
+
+export class Store {
+  private state: AppState
+  private listeners = new Set<Listener>()
+
+  constructor(initial: AppState) {
+    this.state = initial
+  }
+
+  getState(): AppState {
+    return this.state
+  }
+
+  setState(partial: Partial<AppState>): void {
+    this.state = { ...this.state, ...partial }
+    this.listeners.forEach(fn => fn(this.state))
+  }
+
+  subscribe(listener: Listener): () => void {
+    this.listeners.add(listener)
+    return () => this.listeners.delete(listener)
+  }
+}
